58cd0f5b2b6dc3ede041a334fee23281fb665139
CristianClaudio <eugenio.feliz15@gmail.com>
2026-09-21 20:15:06 -0400

    feat: modulo de citas con inyeccion de PacientesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PacientesModule } from './pacientes/pacientes.module';
 import { MedicosModule } from './medicos/medicos.module';
+import { CitasModule } from './citas/citas.module';
 
 @Module({
-  imports: [PrismaModule, PacientesModule, MedicosModule],
+  imports: [PrismaModule, PacientesModule, MedicosModule, CitasModule],
   controllers: [AppController],
   providers: [AppService],
 })
